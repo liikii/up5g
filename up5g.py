0cf3e14aef60dbcb338ef6f22aab3f39feb7b614
liikii <609957891@qq.com>
2018-04-17 13:05:42 +0800

    up5g

---
+++ up5g.py
@@ -0,0 +1,89 @@
+import tornado.ioloop
+import tornado.web
+from tornado import options
+from tornado.httpserver import HTTPServer
+
+
+from urllib.parse import unquote
+
+
+page = """<!DOCTYPE html>
+<html>
+<body>
+
+<form action="/up" method="post" enctype="multipart/form-data">
+  Select images: <input type="file" name="files" multiple/>
+  <input type="submit" value='up it'>
+</form>
+
+<p>Try selecting more than one file when browsing for files.</p>
+
+<p><strong>Note:</strong> The multiple attribute of the input tag is not supported in Internet Explorer 9 and earlier versions.</p>
+
+</body>
+</html>"""
+
+
+def handle_payload(self, chunk):
+    pass
+
+
+@tornado.web.stream_request_body
+class PUTHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.bytes_read = 0
+        print(self.request)
+        print(id(self))
+
+    def data_received(self, chunk):
+        if self.bytes_read == 0:
+            print(self.request.headers)
+            # print(chunk)
+        # print('get chunk')
+        print(chunk)
+        self.bytes_read += len(chunk)
+
+    def post(self):
+        # filename = unquote(filename)
+        mtype = self.request.headers.get('Content-Type')
+        # print('post "%s" "%s" %d bytes', 'file_name', mtype, self.bytes_read)
+        # print(self.request.headers)
+        self.write('OK')
+
+    def get(self):
+        self.write(page)
+
+    def on_finish(self):
+        print('finish..')
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(page)
+
+    def post(self):
+        # self.write(page)
+        return
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/up", PUTHandler),
+        (r"/.*", IndexHandler)
+    ], autoreload=True)
+
+
+# curl -vX POST --data-binary @cache.zip http://192.168.1.200:8888
+# url -vX POST --data-binary @cache.zip http://127.0.0.1:8888
+if __name__ == "__main__":
+    # Tornado configures logging.
+    # options.parse_command_line()
+    app = make_app()
+    max_buffer_size = 10 * 1024 ** 5  # 4GB
+    http_server = HTTPServer(
+        app,
+        max_buffer_size=max_buffer_size,
+    )
+    http_server.listen(8888)
+    # app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
